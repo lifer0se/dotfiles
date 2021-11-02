d2089a5964f63044a75ee28c76a6cf978b5a9810
lifer0se <a.nikoletopoulos@gmail.com>
2021-11-02 03:55:22 +0200

    Delete .scripts directory

--- .scripts/suckless_update_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BUILDPATH=~/.gitclones/$1
-[[ ! $1 ]] && BUILDPATH=~/.gitclones/dwm
-[[ ! -d $BUILDPATH ]] && echo "\"$1\" does not exists in ~/.gitclones." && exit
-cd $BUILDPATH
-sudo cp config.def.h config.h
-sudo make CDBG=-w clean install
